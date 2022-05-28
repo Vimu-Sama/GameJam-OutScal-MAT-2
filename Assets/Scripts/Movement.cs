3e2fe6bec60f5388d4154d92ca6de758764a75ee
Vimarsh Sharma <42619785+Vimu-Sama@users.noreply.github.com>
2022-05-28 16:07:32 +0530

    basic movements for player

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [Header("Physics Quantities")]
+    [SerializeField] float moveSpeed;
+    [SerializeField] float dashForce= 1f;
+    [Header("Connected GameObjects")]
+
+    bool jump= true;
+    Vector2 pos = new Vector2(0, 0);
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector2(transform.position.x + (pos.x * moveSpeed * Time.fixedDeltaTime),
+                                transform.position.y + (pos.y * moveSpeed * Time.fixedDeltaTime));
+        pos = new Vector2(0, 0);
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            pos = new Vector2(-1, 0);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            pos = new Vector2(1, 0);
+        }
+        else if(Input.GetKey(KeyCode.W))
+        {
+            pos = new Vector2(0, 1);
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            pos = new Vector2(0, -1);
+        }
+
+    }
+}
