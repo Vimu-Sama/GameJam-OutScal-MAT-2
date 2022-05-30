8daedf8ddbeba841478b9be3d7ec504fe1f420f5
Vimarsh Sharma <42619785+Vimu-Sama@users.noreply.github.com>
2022-05-31 00:30:37 +0530

    Added player cursor follow and shoot in that direction

--- Assets/Scripts/AimNdShoot.cs
+++ Assets/Scripts/AimNdShoot.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class AimNdShoot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   public Camera cam;
+   [SerializeField] GameObject bullet;
+    [SerializeField] Transform bulletPos; //bullets instantiating position
+    [SerializeField] float bulletForce= 10f;
+    Vector2 direction= new Vector2();
+    [HideInInspector] public Vector2 bulletDirection= new Vector2();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   void Update()
+   {
+        Vector3 mousePos= Input.mousePosition;
+        mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+        direction= new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y) ;
+        transform.up = direction;
+        if(Input.GetMouseButtonDown(0))  //if pressed left mouse key
+        {
+            GameObject instance =Instantiate(bullet, bulletPos.position, bulletPos.rotation);
+            instance.GetComponent<Rigidbody2D>().AddForce(bulletPos.up * bulletForce, ForceMode2D.Impulse);
+        }
+   }
 }
